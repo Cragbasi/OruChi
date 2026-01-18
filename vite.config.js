9bd7eaee7c55e60ae0431fb736bd5158a067dfd3
Chi <cragbasi@gmail.com>
2026-01-18 00:03:53 -0700

    changed homepage name in vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "news_explorer", // add this field
+  base: "OruChi", // add this field
   plugins: [react()],
   server: {
     port: 3000,
